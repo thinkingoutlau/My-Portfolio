15c2778d7677c73b7a00a69cc24de9fd6ffb7349
Winnie Lau <thinkingoutlau@gmail.com>
2022-11-02 00:04:11 -0400

    created react app without cra

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  entry: ["./src/index.js"],
+  output: {
+    path: __dirname,
+    filename: "./public/bundle.js",
+  },
+  mode: "development",
+  context: __dirname,
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["@babel/preset-react"],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
