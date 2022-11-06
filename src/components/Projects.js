f668115a6f68d4c4a62efcdb12d06aaf84d44d66
Winnie Lau <thinkingoutlau@gmail.com>
2022-11-06 14:34:47 -0500

    added projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,29 @@
 import React from "react";
+import projects from "../projects";
 
 const Projects = () => {
-  return <div>Hello</div>;
+  console.log(projects);
+  return (
+    <div className="projects">
+      <h2>
+        <u>Projects</u>
+      </h2>
+      <br />
+      <div className="project">
+        {projects.map((project) => {
+          return (
+            <div key={project.id}>
+              <div>
+                <h3>{project.title}</h3>
+              </div>
+              <div>{project.description}</div>
+              <div>{project.link}</div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Projects;
