76f816511452ee2ce921d6cd40ce3764f8e64cd0
Winnie Lau <thinkingoutlau@gmail.com>
2022-11-08 12:43:01 -0500

    edited aboutMe

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -8,10 +8,12 @@ const Profile = () => {
         <img className="picture" src="/Winnie_Lau.png" />
       </div>
       <div className="aboutMe">
-        Hi there! I'm Winnie :) I am a software engineer and was a former
-        educator. I was a high school mathematics teacher and a college
-        lecturer. I had recently transitioned into the tech world and have been
-        loving every minute of it.
+        Hi there! I'm Winnie and I'm from NYC :) I am a software engineer 👩🏻‍💻 and
+        was a former educator 👩🏻‍🏫. I was a high school mathematics teacher and a
+        college lecturer. My passions were always mathematics and education. It
+        wasn't until I came upon this new world that I discovered coding will
+        become another passion of mine. I recently transitioned into the tech
+        world and have been loving every minute of it!
       </div>
       <br />
 
